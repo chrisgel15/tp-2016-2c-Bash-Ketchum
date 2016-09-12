6a6631c32b00db818e4bcc523fde251638ca14ad
federicomandri <fede_mandri@yahoo.com.ar>
2016-09-12 17:02:21 -0300

    SRDF - Proyecto auxiliar para implementar algoritmo srdf.

---
+++ SRDF/src/Srdf.c
@@ -0,0 +1,81 @@
+/*
+ ============================================================================
+ Name        : Srdf.c
+ Author      : Federico Mandri
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+
+#include "Srdf.h"
+
+void crearEntrenador(t_list*,char,int,int);
+void crearPokeNest(t_list*,char,int,int,int);
+void inicializar_colas(void);
+void inicializar_listas(void);
+t_entrenador* inicializar_info_entrenador(char*, char, int, int);
+//void srdf(t_queue);
+
+
+t_list* cola_listos;
+t_queue* cola_ejecucion;
+t_list* entrenadores;
+t_list* items;
+
+int main(void) {
+	int filas, columnas;
+	inicializar_colas();
+	inicializar_listas();
+
+	crearPokeNest(items,'P',10,10,5);
+	list_add(cola_listos,(void*)inicializar_info_entrenador("Ash", '@', 1, 1));
+	list_add(cola_listos,(void*)inicializar_info_entrenador("Gary", '#', 20, 20));
+	//list_add_all(items,cola_listos);
+	crearEntrenador(items,'@',1,1);
+	CrearEnemigo(items,'#',20,20);
+	nivel_gui_inicializar();
+	nivel_gui_get_area_nivel(&filas, &columnas);
+	nivel_gui_dibujar(items, "Test SRDF");
+	sleep(5);
+	nivel_gui_terminar();
+	//srdf(cola_listos);
+
+
+	return EXIT_SUCCESS;
+}
+
+
+void crearEntrenador(t_list* items, char id, int x, int y){
+	CrearPersonaje(items,id,x,y);
+}
+
+void crearPokeNest(t_list* items, char id, int x, int y, int cantidad){
+	CrearCaja(items,id,x,y,cantidad);
+}
+
+void inicializar_colas(void){
+	cola_ejecucion = queue_create();
+
+}
+
+void inicializar_listas(void){
+	cola_listos = list_create();
+	entrenadores = list_create();
+	items = list_create();
+}
+
+t_entrenador* inicializar_info_entrenador(char* nombre, char id, int x, int y){
+	t_entrenador* nuevo_entrenador = malloc(sizeof(t_entrenador));
+	nuevo_entrenador->nombre = nombre;
+	nuevo_entrenador->id = id;
+	nuevo_entrenador->posx = x;
+	nuevo_entrenador->posy = y;
+
+	return nuevo_entrenador;
+}
+
+//void srdf(t_queue entrenadores_listos){
+//
+//}
---
+++ SRDF/src/Srdf.h
@@ -0,0 +1,28 @@
+/*
+ * Srdf.h
+ *
+ *  Created on: 12/9/2016
+ *      Author: utnso
+ */
+
+#ifndef SRDF_H_
+#define SRDF_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/collections/list.h>
+#include <commons/collections/queue.h>
+#include <curses.h>
+#include <nivel.h>
+#include <tad_items.h>
+#include <unistd.h>
+
+typedef struct{
+	char* nombre;
+	char id;
+	int posx;
+	int posy;
+}t_entrenador;
+
+
+#endif /* SRDF_H_ */
