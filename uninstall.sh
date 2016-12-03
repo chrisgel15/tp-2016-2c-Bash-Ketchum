98f17c3c04739fb001d903bdecb13ffd02c85735
alexsem <asemprini87@gmail.com>
2016-12-03 15:04:36 -0300

    si no le pongo chmod +x es un poco dificil que ejecute

--- uninstall.sh
+++ uninstall.sh
@@ -8,8 +8,6 @@ cd /home/utnso
 
 sudo rm -rf so-commons-library/
 
-sudo rm -rf /home/utnso/tp-2016-2c-Bash-Ketchum
-
 cd /home/utnso/massive-file-creator
 
 make clean
@@ -19,5 +17,7 @@ cd /home/utnso
 sudo rm -rf massive-file-creator/
 
 sudo rm -rf osada-utils/
+
+sudo rm -rf /home/utnso/tp-2016-2c-Bash-Ketchum
 
 export LD_LIBRARY_PATH = ""
